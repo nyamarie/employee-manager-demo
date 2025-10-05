bb8dc5e1a73c762b8f410c278b6b5343f1f6670a
Nya <nyawilliams@usf.edu>
2025-10-05 19:47:25 -0400

    step 2

---
+++ cc_6.js
@@ -0,0 +1,12 @@
+// Base class Employee
+class Employee {
+    constructor(name, department) {
+    this.name = name;
+    this.department = department;
+    }
+    
+    
+    describe() {
+    return `${this.name} works in the ${this.department} department.`;
+    }
+    }
