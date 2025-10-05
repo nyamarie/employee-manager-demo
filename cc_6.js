194af58a9cbd6bf9115d5df937eff67c688612d2
Nya <nyawilliams@usf.edu>
2025-10-05 19:48:09 -0400

    step 3

--- cc_6.js
+++ cc_6.js
@@ -10,3 +10,16 @@ class Employee {
     return `${this.name} works in the ${this.department} department.`;
     }
     }
+    // Subclass Manager extends Employee
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+    super(name, department); // inherit name & department
+    this.teamSize = Number(teamSize);
+    }
+    
+    
+    // Method overriding: add manager-specific info
+    describe() {
+    return `${this.name} manages a team of ${this.teamSize} in the ${this.department} department.`;
+    }
+    }
